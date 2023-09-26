4a4e37061868c92476ee4ed1e9f29d4385adaff2
DumitruConstantinoCristian <144817378+DumitruConstantinoCristian@users.noreply.github.com>
2023-09-26 14:26:34 +0100

    Add files via upload

---
+++ PythonApplication5/PythonApplication5.py
@@ -0,0 +1,17 @@
+print("Hello my name is Susan from Space")
+print("What is your name?")
+input1= input("Enter name")
+print(f"Hello {input1} I am from the year 2210 and im 20 years old")
+print("How old are you?")
+number_1= input('enter a number')
+number_2 = 2210
+int(number_1) + int(number_2)
+result= float(number_1) + float(number_2)
+print (f'wow, by 2010 you will be {result} that is really old!')
+print("What music are you in to?")
+input3=input("enter music type")
+print(f"I have not heard of {input3}")
+
+
+
+
